feca915c5b531f6b5942d740a1972364250a190f
Fábio Neves <fabiogarcia92@gmail.com>
2017-04-28 11:24:50 -0300

    Update PartsController.php

--- src/MyApp/Controller/PartsController.php
+++ src/MyApp/Controller/PartsController.php
@@ -12,7 +12,7 @@ class PartsController {
         $conn = $app['db'];
         $nome = $request->get('nome');
         $opcional = $request->get('opcional');
-        $conn->update('public.parts', array('nome' => $nome, 'opcional' => $opcional), array('part_id' => $id));
+        $conn->update('parts', array('nome' => $nome, 'opcional' => $opcional), array('part_id' => $id));
         try {
             
         } catch (\Exception $e) {
@@ -29,7 +29,7 @@ class PartsController {
     public function show(Application $app, $id) {
         $conn = $app['db'];
 
-        $sql = "SELECT * FROM public.parts WHERE car_id = ?";
+        $sql = "SELECT * FROM parts WHERE car_id = ?";
         $statement = $conn->executeQuery($sql, array($id));
         $parts = $statement->fetchAll();
         return new JsonResponse(
@@ -40,7 +40,7 @@ class PartsController {
     public function select(Application $app, $id) {
         $conn = $app['db'];
 
-        $sql = "SELECT * FROM public.parts WHERE part_id = ?";
+        $sql = "SELECT * FROM parts WHERE part_id = ?";
         $statement = $conn->executeQuery($sql, array($id));
         $parts = $statement->fetch();
         return new JsonResponse(
@@ -53,7 +53,7 @@ class PartsController {
         $nome = $request->get('nome');
         $opcional = $request->get('opcional');
         try {
-            $conn->insert('public.parts', array('nome' => $nome, 'opcional' => $opcional, 'car_id' => $carro));
+            $conn->insert('parts', array('nome' => $nome, 'opcional' => $opcional, 'car_id' => $carro));
         } catch (\Exception $e) {
             return new JsonResponse(
                     array('message' => 'Não foi!'), 500
@@ -67,7 +67,7 @@ class PartsController {
     public function delete(Application $app, $id) {
         $conn = $app['db'];
         try {
-            $conn->delete('public.parts', array('part_id' => $id));
+            $conn->delete('parts', array('part_id' => $id));
         } catch (\Exception $e) {
             return new JsonResponse(
                     array('message' => 'Não foi!'), 500
